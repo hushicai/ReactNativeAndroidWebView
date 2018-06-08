b2bfb97b9942d9e968311160c007e7c934cd1eea
hushicai <hushicai@gf.com.cn>
2018-06-08 23:56:37 +0800

    react native upgrade to 0.50.0

--- android/app/src/main/java/com/reactnativeandroidwebview/MainApplication.java
+++ android/app/src/main/java/com/reactnativeandroidwebview/MainApplication.java
@@ -27,6 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
           new CustomWebViewPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
